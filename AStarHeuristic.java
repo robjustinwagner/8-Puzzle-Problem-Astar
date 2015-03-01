366f0e58d48c53f35d75f15c4dd383eefc7b3c53
Robert Wagner <robjustinwagner@users.noreply.github.com>
2015-03-01 17:24:50 -0600

    Update AStarHeuristic.java

--- AStarHeuristic.java
+++ AStarHeuristic.java
@@ -1,4 +1,11 @@
-
+/* An interface for the A* Heuristic. 
+ * @author Bob (Robert) Wagner
+ */
 public interface AStarHeuristic {
+	
+	/* This method must be implemented to retrieve the cost 
+	 * of reaching the goal board state from the current
+	 * board state.
+	 */
 	public int getCost(Board state, Board goalState);
 }
