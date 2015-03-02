24e7f5d69ad9fc4776220dd0bb95b75b50e52e74
Robert Wagner <robjustinwagner@users.noreply.github.com>
2015-03-01 18:54:09 -0600

    Update Frontier.java

--- Frontier.java
+++ Frontier.java
@@ -1,33 +1,41 @@
-
+/* This class represents the Frontier of
+ * the A* Search algorithm implemented in
+ * AStar.java; it holds the current
+ * board state we are evaluating as 
+ * possible next state.
+ *
+ * @author Bob (Robert) Wagner
+ */
 public class Frontier {
 	
-	private Board wrapperBoard;
-	private int wrapperCost = 0;
-	private int wrapperSortVal;
+	private Board wrapperBoard;	//board under evaluation
+	private int wrapperCost = 0;	//current cost of reaching the board
+	private int wrapperSortVal;	//the sorting value of the board
 	
+	//constructor
 	public Frontier(Board board, int sortVal, int cost) {
 		wrapperBoard = board;
 		wrapperSortVal = sortVal;
 		wrapperCost = cost;
 	}
 	
-	public Board getBoard(){
+	public Board getBoard() {
 		return wrapperBoard;
 	}
 	
-	public void setBoard(Board board){
+	public void setBoard(Board board) {
 		wrapperBoard = board;
 	}
 	
-	public int getCost(){
+	public int getCost() {
 		return wrapperCost;
 	}
 	
-	public void setCost(int cost){
+	public void setCost(int cost) {
 		wrapperCost = cost;
 	}
 	
-	public void setSortVal(int val){
+	public void setSortVal(int val) {
 		wrapperSortVal = val;
 	}
 	
